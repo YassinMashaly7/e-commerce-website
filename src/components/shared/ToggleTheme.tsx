88cb5ebeb3c47432e373359999113abb97ff92cb
YassinMashaly7 <yassinmashaly7@gmail.com>
2025-03-31 01:51:22 +0200

    Setting up dark mode

---
+++ src/components/shared/ToggleTheme.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+
+export default function ToggleTheme() {
+  const { theme, setTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, [isMounted]);
+
+  if (!isMounted) return null;
+
+  console.log(theme);
+
+  return (
+    <Button
+      variant="ghost"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      {theme === "light" ? <Moon /> : <Sun />}
+    </Button>
+  );
+}
